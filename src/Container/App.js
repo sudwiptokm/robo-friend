5e2ef5cf63550fe25ff438366096806dd70ec7de
sudwiptokm <94325115+sudwiptokm@users.noreply.github.com>
2021-12-05 00:42:38 +0600

    Used Hooks

--- src/Container/App.js
+++ src/Container/App.js
@@ -1,47 +1,40 @@
-import React, {Component} from 'react';
+import React, {useEffect, useState} from 'react';
 import CardList from '../Components/CardList';
 import SearchBox from '../Components/SearchBox';
 import "./App.css"
 import Scroll from "../Components/Scroll"
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots : [],
-      searchfield : ""
-    }
-  }
+function App () {
 
-  componentDidMount(){
+  const [robots, setRobots] = useState([])
+  const [searchfield, setSearchfield] = useState("")
+
+  useEffect(()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
     .then(response => response.json())
-    .then(user => this.setState({robots: user}))
-  }
+    .then(user => setRobots(user))
+  },[])
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})
+  const onSearchChange = (event) => {
+    setSearchfield(event.target.value)
     
   }
-  render(){
-    const filteredRobots = this.state.robots.filter(robot =>{
-      return robot.name.toLocaleLowerCase().includes(this.state.searchfield.toLocaleLowerCase())
-    })
-    if (this.state.robots.length === 0){
-      return <h1>Loading</h1>
-    }
-    else{
-      return(
-        <div className="tc">
-          <h1 className="ttu f1"> Robo Friends </h1>
-          <SearchBox searchchange = {this.onSearchChange}/>
-          <Scroll>
-            <CardList robots = {filteredRobots} />
-          </Scroll>
-        </div>
-      )
-    }
-    
+  const filteredRobots = robots.filter(robot =>{
+    return robot.name.toLocaleLowerCase().includes(searchfield.toLocaleLowerCase())
+  })
+  if (robots.length === 0){
+    return <h1>Loading</h1>
+  }
+  else{
+    return(
+      <div className="tc">
+        <h1 className="ttu f1"> Robo Friends </h1>
+        <SearchBox searchchange = {onSearchChange}/>
+        <Scroll>
+          <CardList robots = {filteredRobots} />
+        </Scroll>
+      </div>
+    )
   }
 }
 
